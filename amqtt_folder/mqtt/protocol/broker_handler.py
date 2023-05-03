b11b99c276e31aea581d5bb4a57a1a3d7031e06c
burcutanis <burcutanis@sabanciuniv.edu>
2023-05-03 23:20:23 +0300

    last

--- amqtt_folder/mqtt/protocol/broker_handler.py
+++ amqtt_folder/mqtt/protocol/broker_handler.py
@@ -922,6 +922,7 @@ class BrokerProtocolHandler(ProtocolHandler):
             suback = SubackPacket.build(packet_id, return_codes, None) #build the packet in its default version
 
 
+
         await self._send_packet(suback)
 
     async def mqtt_acknowledge_unsubscription(self, packet_id):
